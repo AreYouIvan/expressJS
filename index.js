375645d49fb1f458a99b7d2b478ac8156a437997
Ivan Garcia <ivangm180@gmail.com>
2021-10-06 08:00:37 -0500

    Adding pages to the route

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ app.get('/', (req, res) => {
   res.send('Hola mundo!');
 });
 
+app.get('/home', (req, res) => {
+  res.send('Que tal! estás en el home de la aplicación.');
+});
+
+app.get('/categories', (req, res) => {
+  res.send('Estas son las categorias de la aplicación.');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'product',
+      price: 1000,
+    },
+    {
+      name: 'product 2',
+      price: 1399,
+    },
+    {
+      name: 'product 3',
+      price: 799,
+    },
+  ]);
+});
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 });

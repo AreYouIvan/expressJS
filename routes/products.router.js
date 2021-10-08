72419256baf8c10f3e7880ebc9ca492d7b87870c
Ivan Garcia <ivangm180@gmail.com>
2021-10-08 08:23:17 -0500

    making the delete method and the CRUD is complete.

--- routes/products.router.js
+++ routes/products.router.js
@@ -42,8 +42,17 @@ router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
   res.json({
-    message: 'created',
+    message: 'updated',
     data: body,
+    id,
+  });
+});
+
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
     id,
   });
 });

8e038745e57e31d2c8f666fc17140ba61e044fdd
Ivan Garcia <ivangm180@gmail.com>
2021-10-14 07:34:44 -0500

    making middlewares

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // Calling the library.
 const express = require('express');
 const routerApi = require('./routes');
+
+const { logErrors, errorHandler } = require('./middlewares/error.hander');
+
 const app = express();
 const port = 3001;
 
-app.use(express.json()); 
+app.use(express.json());
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {});
 
-routerApi(app);
+
+

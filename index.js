1740ca4b20d11148848064bea68b8e580e79fa3e
Ivan Garcia <ivangm180@gmail.com>
2021-10-15 10:07:54 -0500

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Calling the library.
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const {
   logErrors,
@@ -9,10 +10,22 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
-
+// Making a whitelist to get the api CORS
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not enabled'));
+    }
+  },
+};
+// CORS with whitelist
+app.use(cors(options));
 routerApi(app);
 
 app.use(logErrors);

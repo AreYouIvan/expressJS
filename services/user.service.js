6c401867bc92b674232dffd69e89df8b404d134a
Ivan Garcia <ivangm180@gmail.com>
2022-01-17 08:42:07 -0600

    [ADD] removing faker

--- services/user.service.js
+++ services/user.service.js
@@ -1,63 +1,28 @@
-const faker = require('faker');
-const getConnection = require('../libs/postgres')
-const boom = require('@hapi/boom')|
+const getConnection = require('../libs/postgres');
+const boom = require('@hapi/boom');
 
 
 class UsersService {
-  constructor() {
-    this.users = [];
-    this.generate();
-  }
-
-  generate() {
-    const limit = 100;
-    for (let index = 0; index < limit; index++) {
-      this.users.push({
-        id: faker.datatype.uuid(),
-        name: faker.name.firstName(),
-        email: faker.internet.email(),
-      });
-    }
-  }
+  constructor() {}
 
   async find() {
     const client = await getConnection();
-    const response = await client.query('SELECT * FROM public.tasks');
-    return response.rows;
+    const res = await client.query('SELECT * FROM public.tasks');
+    return res.rows;
   }
 
-  findOne(id) {
-    return this.users.find((item) => item.id === id);
+  async findOne(id) {
+    return { id };
   }
-  create(data) {
-    const newUser = {
-      userId: faker.datatype.uuid(),
-      ...data,
-    };
-    this.users.push(newUser);
-    return this.users;
+  async create(data) {
+    return data;
   }
 
-  update(id, changes) {
-    const index = this.users.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new Error('User not found');
-    }
-    const user = this.users[index];
-    this.users[index] = {
-      ...user,
-      ...changes,
-    };
-    return this.users[index];
+  async update(id, changes) {
+    return { id, changes };
   }
 
-  delete(id) {
-    const index = this.users.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new Error('User not found');
-    }
-    // const user = this.users;
-    this.users.splice(index, 1);
+  async delete(id) {
     return { id };
   }
 }

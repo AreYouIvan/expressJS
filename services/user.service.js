d0d576a50c997e8e82f76fb18435484b4081b624
Ivan Garcia <ivangm180@gmail.com>
2022-01-27 08:17:38 -0600

    [ADD] Creating the first model with Sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,6 @@
 // const boom = require('@hapi/boom');
 const pool = require('../libs/postgres.pool');
+const { models } = require('./../libs/sequelize')
 
 class UserService {
   constructor() {
@@ -13,8 +14,8 @@ class UserService {
   }
 
   async find() {
-    const rta = await this.pool.query('SELECT * FROM public.tasks');
-    return rta.rows;
+    const rta = await models.User.findAll();
+    return rta;
   }
 
   async findOne(id) {

95479fc66576c8997d4ee0c4eb4e129ce5b3da84
Ivan Garcia <ivangm180@gmail.com>
2022-01-27 08:18:39 -0600

    [ADD] Creating the first model with Sequelize

---
+++ db/models/index.js
@@ -0,0 +1,8 @@
+// This file is the model's setup
+const { User, UserSchema } = require('./user.model');
+
+function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize));
+}
+
+module.exports = setupModels;

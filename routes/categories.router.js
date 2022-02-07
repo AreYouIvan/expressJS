45250d74685033bfa6dd8556455d0d9a5b0a4b82
Ivan Garcia <ivangm180@gmail.com>
2022-02-07 09:21:20 -0600

    [ADD/FIX] Adding new tables migrations (products with categories 1->many) and Fixing the issue with the primary key.

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,51 +1,73 @@
 const express = require('express');
+
+const CategoryService = require('./../services/category.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
+
 const router = express.Router();
+const service = new CategoryService();
 
-// Catching the params by url and print they in return.
-router.get('/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
-
-router.post('/:categoryId', (req, res) => {
-  const body = req.body;
-  res.json({
-    message: 'Category created',
-    body,
-  });
-});
-// Delete
-router.delete('/:categoryId', (req, res) => {
-  const { categoryId } = req.params;
-  res.json({
-    message: 'Category deleted',
-    categoryId,
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
 });
 
-// Put
-router.put('/:categoryId', (req, res) => {
-  const { categoryId } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'Category updated',
-    data: body,
-    categoryId,
-  });
-});
+router.get('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-// Patch
-router.patch('/categoryId', (req, res) => {
-  const { categoryId } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'Category updated',
-    data: body,
-    categoryId,
-  });
-});
+router.post('/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
--- schemas/category.schema.js
+++ schemas/category.schema.js
@@ -1,22 +1,28 @@
+// dto = data transfer offer.
 const Joi = require('joi');
 
-const id = Joi.number().integer();
+const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
+const price = Joi.number().integer().min(10);
+const description = Joi.string().min(15);
 const image = Joi.string().uri();
 
-const createCategorySchema = Joi.object({
+const createProductSchema = Joi.object({
   name: name.required(),
-  image: image.required()
+  price: price.required(),
+  description: description.required(),
+  image: image.required(),
 });
 
-const updateCategorySchema = Joi.object({
+const updateProductSchema = Joi.object({
   name: name,
-  image: image
+  price: price,
+  image: image,
+  description
 });
 
-const getCategorySchema = Joi.object({
+const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createCategorySchema, updateCategorySchema, getCategorySchema }
-
+module.exports = { createProductSchema, updateProductSchema, getProductSchema };

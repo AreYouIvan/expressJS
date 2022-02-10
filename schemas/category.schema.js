c073547868530a244c040819b88d6b2eb8942b39
Ivan Garcia <ivangm180@gmail.com>
2022-02-10 07:21:13 -0600

    [FIX] Solving issue with category and products. The error was a bad export form category.schema

--- schemas/category.schema.js
+++ schemas/category.schema.js
@@ -5,18 +5,18 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3).max(15);
 const image = Joi.string().uri();
 
-const createProductSchema = Joi.object({
+const createCategorySchema = Joi.object({
   name: name.required(),
   image: image.required()
 });
 
-const updateProductSchema = Joi.object({
+const updateCategorySchema = Joi.object({
   name: name,
   image: image
 });
 
-const getProductSchema = Joi.object({
+const getCategorySchema = Joi.object({
   id: id.required()
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema };
+module.exports = { createCategorySchema, updateCategorySchema, getCategorySchema };
